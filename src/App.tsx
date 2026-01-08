d03b5f9fa142957628ca57b217a989158f51dcf5
Moreno Pradita <morenopradita013@gmail.com>
2026-01-09 04:38:31 +0700

    Update Production

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import Clock from './components/Clock'
 import Countdown from './components/Countdown'
 import Documents from './components/Documents'
-import Kurs from './components/Kurs'
+// import Kurs from './components/Kurs'
 import Itinerary from './components/Itinerary'
 import Button from './components/ScrollToTopButton'
 
